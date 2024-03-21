3b0173cfd5ff66a2d37eb08f8bc7c6c88afaeff5
takechi808 <yukitake844@gmail.com>
2024-03-21 22:43:03 +0900

    ロゴの追加

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,56 +7,73 @@ const LoginPage: React.FC = () => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-
+  const handleSubmit = async () => {
     const result = await signIn('credentials', {
       redirect: false, 
       username,
       password,
-      callbackUrl: `${window.location.origin}/home`
+      callbackUrl: `${window.location.origin}/chat`
     });
 
-    if (result && result.url) window.location.href = result.url; // リダイレクト
+    if (result && result.url) window.location.href = result.url;
   };
 
   return (
     <div>
+      <Center>
+        <img src="yumeniki_logo.webp" alt="" width="400"/>
+      </Center>
       <Center h="xl">
         <Container>
           <VStack>
-            <Spacer />
 
             <Center>
-              <Text
+            <Text
               fontSize="xl"
               fontWeight="bold"
               bgGradient="linear(to-l, #7928CA, #FF0080)"
               bgClip="text"
               >
-                Welcome to 夢ニキ
-              </Text>
+              夢ニキ
+            </Text>
             </Center>
 
             <Center>
-              <Button colorScheme="fuchsia"  variant="outline" onClick={() => signIn()}>Sign In</Button>
-            </Center>
 
+            <Input
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              />
+              </Center>
+
+            <Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
+
+            <Center>
+              <Button colorScheme="fuchsia" variant="outline" onClick={handleSubmit}>Sign In</Button>
+            </Center>
+            
             <Center>
               <Text 
                 fontSize="xs"
                 fontWeight="bold"
                 bgGradient="linear(to-l, #7928CA, #FF0080)"
                 bgClip="text"
-              >
+                >
                 Don't have an account? Sign up <Link href="/signup">here</Link>
               </Text>
             </Center>
+          
           </VStack>
         </Container>
       </Center>
     </div>
   );
-}
+};
 
 export default LoginPage;
